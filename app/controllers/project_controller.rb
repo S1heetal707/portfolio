3a36c9b357d168ed4b4de02f1db5b405ca0bf701
S1heetal707 <sheetalsawkare730@gmail.com>
2024-09-04 17:45:21 +0530

    added images and icons

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -3,6 +3,25 @@ class ProjectController < ApplicationController
 		@projects = Project.all
 	end
 
+	def create
+	  project = Project.new(project_params)
+	  
+	  if project.save
+	    if params[:project][:image].present?
+	      project.image.attach(params[:project][:image])
+	      if project.image.attached?
+	        render json: { message: "Project Created Successfully" }, status: :created
+	      else
+	        render json: { error: "Image attachment failed" }, status: :unprocessable_entity
+	      end
+	    else
+	      render json: { message: "Project Created Successfully but no image was attached" }, status: :created
+	    end
+	  else
+	    render json: { error: project.errors.full_messages.to_sentence }, status: :unprocessable_entity
+	  end
+	end
+
 	def show
 	end
 
@@ -12,7 +31,6 @@ class ProjectController < ApplicationController
 	private
 
 	def project_params
-		params.require(:project).permit(:title, :description, :technology)
+		params.require(:project).permit(:title, :description, :technology, :image)
 	end
-
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,8 @@
 class Project < ApplicationRecord
-	
+	# has_one_attached :image
+	has_one_attached :image
+
+	def resized_image
+    image.variant(resize_to_limit: [100, 100]).processed
+  end
 end
